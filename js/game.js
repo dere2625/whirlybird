46caaad72c3d143428ab2ea4eb0be4b608bf0ee2
dere2625 <89865727+dere2625@users.noreply.github.com>
2022-08-03 23:10:10 -0500

    randomizing mechanism complete

--- js/game.js
+++ js/game.js
@@ -1,8 +1,5 @@
 jQuery(()=>{
 
-    var something = $('.container:nth-child(3)').position();
-    // console.log(something);
-
     var transformInterval = setInterval(()=>{
         var containers = $('body').children();
         for(let i=1; i<=containers.length; i++){
@@ -11,18 +8,28 @@ jQuery(()=>{
                 {top:0,left:position.left - 5}
             )
             if($(`.container:nth-child(${i})`).position().left + 150 < 0){
-                $(`.container:nth-child(${i})`).offset({top:0,left:$(window).width()+400})
-                $(`.container:nth-child(${i})`).children('.bottom').css("background-color","red")
-                $(`.container:nth-child(${i})`).children('.bottom').css("background-color","red")
+                $(`.container:nth-child(${i})`).offset({top:0,left:$(window).width()+200})
+                var temp = getRandomNumber();
+                console.log();
+                $(`.container:nth-child(${i})`).children('.bottom').css("height", temp+"vh")
+                $(`.container:nth-child(${i})`).children('.top').css("height",(80-temp)+"vh")
             }
         }
-    },50);
+    },20);
 
     $('.container').mouseenter((e)=>{
         clearInterval(transformInterval)
     })
 
-    
+    function getRandomNumber(){
+        var ceiling = 60;
+        var floor = 20;
+        let flr = Math.floor(Math.random() * (ceiling - floor) + floor)
+        if(flr < floor){
+            getRandomNumber();
+        }
+        return flr;
+    }
 
 
 
