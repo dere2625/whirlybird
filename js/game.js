e12db930230b93f94585e0a0eb48a6052f12dfd1
dere2625 <89865727+dere2625@users.noreply.github.com>
2022-08-03 23:42:34 -0500

    game play and scoreboard added

--- js/game.js
+++ js/game.js
@@ -1,24 +1,29 @@
 jQuery(()=>{
-
+    var speed = 20;
+    var score = 0;
     var transformInterval = setInterval(()=>{
         var containers = $('body').children();
         for(let i=1; i<=containers.length; i++){
             var position = $(`.container:nth-child(${i})`).position();
-            $(`.container:nth-child(${i})`).offset(
-                {top:0,left:position.left - 5}
-            )
-            if($(`.container:nth-child(${i})`).position().left + 150 < 0){
-                $(`.container:nth-child(${i})`).offset({top:0,left:$(window).width()+200})
-                var temp = getRandomNumber();
-                console.log();
-                $(`.container:nth-child(${i})`).children('.bottom').css("height", temp+"vh")
-                $(`.container:nth-child(${i})`).children('.top').css("height",(80-temp)+"vh")
+            if(position!=undefined){
+                $(`.container:nth-child(${i})`).offset(
+                    {top:0,left:position.left - 10}
+                )
+                if($(`.container:nth-child(${i})`).position().left + 150 < 0){
+                    score++;
+                    $('.scoreboard h3 span').html(score)
+                    $(`.container:nth-child(${i})`).offset({top:0,left:$(window).width()+200})
+                    var temp = getRandomNumber();
+                    $(`.container:nth-child(${i})`).children('.bottom').css("height", temp+"vh")
+                    $(`.container:nth-child(${i})`).children('.top').css("height",(80-temp)+"vh")
+                }
             }
         }
-    },20);
+    },speed);
 
     $('.container').mouseenter((e)=>{
         clearInterval(transformInterval)
+        $(this).children('.bottom').css("background-color", 'red')
     })
 
     function getRandomNumber(){
